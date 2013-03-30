20f586b846cc064d8adbbd2e9689f10b5d4b3122
Andy Brown <andymoe@gmail.com>
2013-03-30 14:34:00 -0700

    Make the build work with 6.1 sdk and clang for armv7

--- lib/build_ffmpeg.sh
+++ lib/build_ffmpeg.sh
@@ -7,7 +7,7 @@ if [ ! -s $ROOT_PATH/ffmpeg/libswscale ] ; then
 	ln -sf ../libswscale/ $ROOT_PATH/ffmpeg/libswscale
 fi
 
-mkdir -p $LIB_PATH/{i386,iphone3g,iphone3gs}
+mkdir -p $LIB_PATH/{i386,iphone3g,iphone3gs,armv7}
 
 PATH=$PATH:$ROOT_PATH
 
@@ -23,41 +23,64 @@ COMMON_PARAMS="--disable-doc --disable-ffplay --disable-ffserver \
 # Note - to disable optimizations (for debugging, etc.), add the following:
 # --enable-decoder=h263 --enable-decoder=svq3 --disable-optimizations"
 
-cd $LIB_PATH/iphone3gs
-
-../../ffmpeg/configure --enable-cross-compile --arch=arm --target-os=darwin \
---cc=/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin/clang \
---sysroot=/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS4.0.sdk \
---extra-cflags='-arch armv7' --extra-ldflags='-arch armv7' --enable-pic \
-$COMMON_PARAMS
-
+cd $LIB_PATH/armv7
+../../ffmpeg/configure --disable-doc --disable-ffmpeg --disable-ffplay --disable-ffserver --enable-cross-compile --arch=arm \
+--target-os=darwin --cc=clang --as='gas-preprocessor/gas-preprocessor.pl /usr/bin/clang' \
+--sysroot=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS6.1.sdk \
+--cpu=cortex-a8 --extra-cflags='-arch armv7' \
+--extra-ldflags='-arch armv7 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS6.1.sdk' \
+--enable-pic --enable-decoder=rawvideo --disable-asm
 make
 
-cd $LIB_PATH/iphone3g
+# cd $LIB_PATH/armv7s
+# ../../ffmpeg/configure --disable-doc --disable-ffmpeg --disable-ffplay --disable-ffserver --enable-cross-compile --arch=arm \
+# --target-os=darwin --cc=clang --as='gas-preprocessor/gas-preprocessor.pl /usr/bin/clang' \
+# --sysroot=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS6.1.sdk \
+# --cpu=cortex-a8 --extra-cflags='-arch armv7s' \
+# --extra-ldflags='-arch armv7s -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS6.1.sdk' \
+# --enable-pic --enable-decoder=rawvideo --disable-asm
+# make
 
-../../ffmpeg/configure --enable-cross-compile --arch=arm --target-os=darwin \
---cc=/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin/clang \
---sysroot=/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS4.0.sdk \
---extra-cflags='-arch armv6' --extra-ldflags='-arch armv6' --enable-pic \
-$COMMON_PARAMS
-
-make
-
-cd $LIB_PATH/i386
-
-SIMULATOR_CONFIG=""
-
-../../ffmpeg/configure --enable-cross-compile --arch=i386 --target-os=darwin \
---cc=/Developer/Platforms/iPhoneSimulator.platform/Developer/usr/bin/clang \
---sysroot=/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator4.0.sdk \
---extra-cflags='-arch i386' --extra-ldflags='-arch i386' --enable-pic --disable-mmx --disable-mmx2 \
-$COMMON_PARAMS
-
-make
+# 
+# cd $LIB_PATH/iphone3gs
+# 
+# ../../ffmpeg/configure --enable-cross-compile --arch=arm --target-os=darwin \
+# --cc=/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin/clang \
+# --sysroot=/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS4.0.sdk \
+# --extra-cflags='-arch armv7' --extra-ldflags='-arch armv7' --enable-pic \
+# $COMMON_PARAMS
+# 
+# make
+# 
+# cd $LIB_PATH/iphone3g
+# 
+# ../../ffmpeg/configure --enable-cross-compile --arch=arm --target-os=darwin \
+# --cc=/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin/clang \
+# --sysroot=/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS4.0.sdk \
+# --extra-cflags='-arch armv6' --extra-ldflags='-arch armv6' --enable-pic \
+# $COMMON_PARAMS
+# 
+# make
+# 
+# cd $LIB_PATH/i386
+# 
+# SIMULATOR_CONFIG=""
+# 
+# ../../ffmpeg/configure --enable-cross-compile --arch=i386 --target-os=darwin \
+# --cc=/Developer/Platforms/iPhoneSimulator.platform/Developer/usr/bin/clang \
+# --sysroot=/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator4.0.sdk \
+# --extra-cflags='-arch i386' --extra-ldflags='-arch i386' --enable-pic --disable-mmx --disable-mmx2 \
+# $COMMON_PARAMS
+# 
+# make
 
 cd $LIB_PATH
 
+# for l in `echo -ne "libavcodec\nlibavdevice\nlibavformat\nlibavutil\nlibswscale"`; do
+#   lipo -create "i386/$l/$l.a" "iphone3gs/$l/$l.a" "iphone3g/$l/$l.a" -output "$l.a";
+# done
+
 for l in `echo -ne "libavcodec\nlibavdevice\nlibavformat\nlibavutil\nlibswscale"`; do
-	lipo -create "i386/$l/$l.a" "iphone3gs/$l/$l.a" "iphone3g/$l/$l.a" -output "$l.a";
+  lipo -create "armv7/$l/$l.a" -output "$l.a";
 done
 
